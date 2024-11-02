a65367608c3c3a24bb0abb81516c1884bd5d04c6
jordannakamoto <jordannakamoto@gmail.com>
2024-11-02 16:45:14 -0700

    IndexedCube, ExperimentalCube done

--- Assignment-3/ExperimentalCube.js
+++ Assignment-3/ExperimentalCube.js
@@ -1,17 +1,125 @@
-/////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
 //
 //  ExperimentalCube.js
 //
-//  A cube defined ???
+//  A procedurally generated cube
+//  I don't think this is particularly efficient but it's interesting to me
 //
 
 class ExperimentalCube {
     constructor(gl, vertexShader, fragmentShader) {
 
-        // let program = new ShaderProgram(gl, this, vertexShader, fragmentShader);
+        vertexShader = `
+            in vec4 aPosition;
+            in vec4 aColor;
+
+            uniform mat4 P;      // Projection matrix
+            uniform mat4 MV;     // Model-view matrix
+
+            out vec4 vColor;
+            void main() {
+                gl_Position = P * MV * aPosition;
+                vColor = aColor;
+            }
+        `;
+
+        fragmentShader ||= `
+            in vec4 vColor;
+            out vec4 fColor;
+
+            void main() {
+                fColor = gl_FrontFacing ? vColor : vec4(0,0,0,0);
+            }
+        `;
+
+        const size = 0.5;
+
+        // Generate 8 vertices based on binary pattern
+        const vertices = new Float32Array([
+            ...(Array.from({ length: 8 }, (_, i) => [
+                (i & 1 ? size : -size),
+                (i & 2 ? size : -size),
+                (i & 4 ? size : -size)
+            ])).flat()
+        ]);
+
+        // Generate color based on vertex index
+        const colors = new Float32Array(
+            Array.from({ length: 8 }, (_, i) => [
+                (i & 1) ? 1.0 : 0.5,     // R
+                (i & 2) ? 1.0 : 0.5,     // G
+                (i & 4) ? 1.0 : 0.5,     // B
+                1.0                      // A
+            ]).flat()
+        );
+
+        // Generate Indices
+        // Store 4 at a time as a face
+        // Process:
+        // 1 axis at a time
+        // 2 faces on opposite sides of each axis
+        // from the point most relative to this side of the axis, shift bits to just add dimensions, creating a unique enumeration
+        const faces = [];
+        for (let axis = 0; axis < 3; axis++) {
+            // Negative side of the current axis (e.g. bottom)
+            let startInd = 0 << axis;
+            faces.push([
+                startInd,
+                startInd | (1 << ((axis + 1) % 3)),
+                startInd | (1 << ((axis + 1) % 3)) | (1 << ((axis + 2) % 3)), // move along the next and next-next axises to get the third point
+                startInd | (1 << ((axis + 2) % 3))
+            ]);
+        
+            // Positive side of the current axis (e.g. top)
+            startInd = 1 << axis;
+            faces.push([
+                startInd,
+                startInd | (1 << ((axis + 1) % 3)),
+                startInd | (1 << ((axis + 1) % 3)) | (1 << ((axis + 2) % 3)),
+                startInd | (1 << ((axis + 2) % 3))
+            ]);
+        }
+
+        // Group faces into triangles for indexed rendering
+        // Adjusting winding order so all are front-facing
+        const indices = new Uint16Array(
+            faces.flatMap((face, i) => 
+                i % 2 === 0  // Reverse winding order for every other face
+                ? [face[0], face[3], face[2], face[0], face[2], face[1]] : [face[0], face[1], face[2], face[0], face[2], face[3]]
+            )
+        );
+        
+        let program = new ShaderProgram(gl, this, vertexShader, fragmentShader);
+
+        // Using the Attribute class to create:
+        // - position
+        // - color
+        this.aPosition = new Attribute(gl, program, 'aPosition', vertices, 3, gl.FLOAT);
+        this.aColor = new Attribute(gl, program, 'aColor', colors, 4, gl.FLOAT);
+
+        // [INDEX BUFFER - create and bind]
+        this.indexBuffer = new Indices(gl, indices);
+
+        // Locations for updating transformation matrices in `draw`
+        this.program = program;
 
         this.draw = () => {
-            // program.use();
+            this.program.use();
+
+            // Enable position and color attributes
+            this.aPosition.enable();
+            this.aColor.enable();
+
+            console.log(this.indexBuffer);
+
+            // Bind and draw the elements using indexed rendering
+            this.indexBuffer.enable();
+            gl.drawElements(gl.TRIANGLES, this.indexBuffer.count, this.indexBuffer.type, 0);
+            this.indexBuffer.disable();
+
+            // Disable attributes
+            this.aPosition.disable();
+            this.aColor.disable();
         };
     }
-};
+}
--- Assignment-3/IndexedCube.js
+++ Assignment-3/IndexedCube.js
@@ -1,17 +1,105 @@
-/////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
 //
-//  IndexedCube.js
+//  Indexed Cube.js
 //
-//  A cube defined of 12 triangles using vertex indices.
+//  A cube defined by 8 unique vertices and using indexed arrays
 //
 
 class IndexedCube {
     constructor(gl, vertexShader, fragmentShader) {
 
-        // let program = new ShaderProgram(gl, this, vertexShader, fragmentShader);
+        vertexShader = `
+            in vec4 aPosition;
+            in vec4 aColor;
+
+            uniform mat4 P;      // Projection matrix
+            uniform mat4 MV;     // Model-view matrix
+
+            out vec4 vColor;
+            void main() {
+                gl_Position = P * MV * aPosition;
+                vColor = aColor;
+            }
+        `;
+
+        fragmentShader ||= `
+            in vec4 vColor;
+            out vec4 fColor;
+
+            void main() {
+                fColor = gl_FrontFacing ? vColor : vec4(0,0,0,0);
+            }
+        `;
+
+        const s = 0.5;  // Half-size of the cube of side-length 1.0 for centering at origin
+
+        // [Vertices]
+        let positions = new Float32Array([
+            -s, -s,  s,   // 0: left  | bottom | front
+             s, -s,  s,   // 1: right | bottom | front
+             s,  s,  s,   // 2: right | top    | front
+            -s,  s,  s,   // 3: left  | top    | front
+            
+            -s, -s, -s,   // 4: left  | bottom | back
+             s, -s, -s,   // 5: right | bottom | back
+             s,  s, -s,   // 6: right | top    | back
+            -s,  s, -s    // 7: left  | top    | back
+        ]);
+
+        // [Indices] - 12 triangles (36 indices)
+        let indices = new Uint16Array([
+            // Front face
+            0, 1, 2,   0, 2, 3,
+        
+            // Back face
+            4, 7, 6,   4, 6, 5,
+        
+            // Top face
+            3, 2, 6,   3, 6, 7,
+        
+            // Bottom face
+            0, 4, 5,   0, 5, 1,
+        
+            // Right face
+            1, 5, 6,   1, 6, 2,
+        
+            // Left face
+            0, 3, 7,   0, 7, 4
+        ]);
+        
+        // Generate colors for each of the 8 vertices
+        const colors = generateUniqueColors(8);
+        
+        let program = new ShaderProgram(gl, this, vertexShader, fragmentShader);
+
+        // Using the Attribute class to create:
+        // - position
+        // - color
+        this.aPosition = new Attribute(gl, program, 'aPosition', positions, 3, gl.FLOAT);
+        this.aColor = new Attribute(gl, program, 'aColor', colors, 4, gl.FLOAT);
+
+        // [INDEX BUFFER - create and bind]
+        this.indexBuffer = new Indices(gl, indices);
+
+        // Locations for updating transformation matrices in `draw`
+        this.program = program;
 
         this.draw = () => {
-            // program.use();
+            this.program.use();
+
+            // Enable position and color attributes
+            this.aPosition.enable();
+            this.aColor.enable();
+
+            // Bind and draw the elements using indexed rendering
+            this.indexBuffer.enable();
+            console.log(this.indexBuffer)
+            gl.drawElements(gl.TRIANGLES, this.indexBuffer.count, this.indexBuffer.type, 0);
+            this.indexBuffer.disable();
+
+            // Disable attributes
+            this.aPosition.disable();
+            this.aColor.disable();
         };
     }
-};
+}
