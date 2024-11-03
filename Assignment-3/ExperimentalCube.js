7b58d6da399c51a403ba7706aa622a21d8c12873
jordannakamoto <jordannakamoto@gmail.com>
2024-11-02 17:59:02 -0700

    exp cube minified

--- Assignment-3/ExperimentalCube.js
+++ Assignment-3/ExperimentalCube.js
@@ -2,46 +2,25 @@
 //
 //  ExperimentalCube.js
 //
-//  A procedurally generated cube
-//  I don't think this is particularly efficient but it's interesting to me
+//  A procedurally generated cube expressed in binary operations
+//  Not readable or efficient but it's funny to me.
 //
+//  s: 1/2 side length
+//  v: vertices attribute
+//  c: color attribute
+//  i: index buffer
 
 class ExperimentalCube {
-    constructor(gl, vertexShader, fragmentShader) {
-
-        vertexShader = `
-            in vec4 aPosition;
-            in vec4 aColor;
-
-            uniform mat4 P;      // Projection matrix
-            uniform mat4 MV;     // Model-view matrix
-
-            out vec4 vColor;
-            void main() {
-                gl_Position = P * MV * aPosition;
-                vColor = aColor;
-            }
-        `;
-
-        fragmentShader ||= `
-            in vec4 vColor;
-            out vec4 fColor;
-
-            void main() {
-                fColor = gl_FrontFacing ? vColor : vec4(0,0,0,0);
-            }
-        `;
-        const s=.5, g=new ShaderProgram(gl,this,vertexShader,fragmentShader),
+    constructor(gl, vs, fs) {
+        vs = `in vec4 aP; in vec4 aC; uniform mat4 P; uniform mat4 MV; out vec4 vC;void main() { gl_Position = P * MV * aP; vC = aC;}`;
+        fs ||= `in vec4 vC;out vec4 fC; void main() { fC = gl_FrontFacing ? vC : vec4(0,0,0,0);}`;
+        const s=.5, g=new ShaderProgram(gl,this,vs,fs),
         // generate vertices, colors
-        v=new Float32Array([...Array(8)].flatMap((_,i)=>[(i&1?s:-s),(i&2?s:-s),(i&4?s:-s)])), a=new Attribute(gl,g,'aPosition',v,3,gl.FLOAT),
-        c=new Float32Array([...Array(8)].flatMap((_,i)=>[(i&1?1:.5),(i&2?1:.5),(i&4?1:.5),1])), b=new Attribute(gl,g,'aColor',c,4,gl.FLOAT),
-        f=[];
-        for(let x=0;x<3;x++){ // generate faces by enumerating off of each axis
-            let y=1<<((x+1)%3),z=1<<((x+2)%3);
-            f.push([0,y,y|z,z].map(m=>m|0<<x),[1<<x,(1<<x)|y,(1<<x)|y|z,(1<<x)|z]);
-        } // define indices and winding order for the faces
-        const i=new Uint16Array(f.flatMap((q,n)=>n%2?[q[0],q[1],q[2],q[0],q[2],q[3]]:[q[0],q[3],q[2],q[0],q[2],q[1]])),
-              j=new Indices(gl,i);
-        this.draw=()=>{g.use();a.enable();b.enable();j.enable();gl.drawElements(gl.TRIANGLES,j.count,j.type,0);j.disable();a.disable();b.disable()};
+        v = new Attribute(gl,g,'aP',new Float32Array([...Array(8)].flatMap((_,i)=>[(i&1?s:-s),(i&2?s:-s),(i&4?s:-s)])),3,gl.FLOAT),
+        c = new Attribute(gl,g,'aC',new Float32Array([...Array(8)].flatMap((_,i)=>[(i&1?1:s),(i&2?1:s),(i&4?1:s),1])),4,gl.FLOAT),
+        // generate indices as faces by bit shifting around each axis. express winding order by alternating pattern
+        i = new Indices(gl,new Uint16Array([...Array(3)].flatMap((_,x)=>{const y=1<<(x+1)%3,z=1<<(x+2)%3;
+            return[0|0<<x,z|0<<x,y|z|0<<x,0|0<<x,y|z|0<<x,y|0<<x,1<<x,(1<<x)|y,(1<<x)|y|z,1<<x,(1<<x)|y|z,(1<<x)|z]})));
+        this.draw=()=>{g.use();v.enable();c.enable();i.enable();gl.drawElements(gl.TRIANGLES,i.count,i.type,0);i.disable();v.disable();c.disable()};
         }
 }
