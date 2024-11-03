4c98e02c2b928e09a950897da239e698bd2a9ead
jordannakamoto <jordannakamoto@gmail.com>
2024-11-02 21:33:26 -0700

    adjust comment

--- Assignment-3/ExperimentalCube.js
+++ Assignment-3/ExperimentalCube.js
@@ -18,7 +18,7 @@ class ExperimentalCube {
         // generate vertices, colors
         v = new Attribute(gl,g,'aP',new Float32Array([...Array(8)].flatMap((_,i)=>[(i&1?s:-s),(i&2?s:-s),(i&4?s:-s)])),3,gl.FLOAT),
         c = new Attribute(gl,g,'aC',new Float32Array([...Array(8)].flatMap((_,i)=>[(i&1?1:s),(i&2?1:s),(i&4?1:s),1])),4,gl.FLOAT),
-        // generate indices as faces by bit shifting around each axis. express winding order by alternating pattern
+        // generate indices as faces by bit shifting around each axis. express winding order by alternating pattern based on the current axis, x
         i = new Indices(gl,new Uint16Array([...Array(3)].flatMap((_,x)=>{const y=1<<(x+1)%3,z=1<<(x+2)%3;
             return[0|0<<x,z|0<<x,y|z|0<<x,0|0<<x,y|z|0<<x,y|0<<x,1<<x,(1<<x)|y,(1<<x)|y|z,1<<x,(1<<x)|y|z,(1<<x)|z]})));
         this.draw=()=>{g.use();v.enable();c.enable();i.enable();gl.drawElements(gl.TRIANGLES,i.count,i.type,0);i.disable();v.disable();c.disable()};
